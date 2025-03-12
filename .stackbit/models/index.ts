685da3c7f2a77ba636b9a6d274dafc0729638a1f
Coen Warmer <coen.warmer@gmail.com>
2025-03-12 23:26:25 +0100

    Further cleanup

---
+++ .stackbit/models/index.ts
@@ -0,0 +1,19 @@
+import { Button } from './Button';
+import { Config } from './Config';
+import { Footer } from './Footer';
+import { Header } from './Header';
+import { Image } from './Image';
+import { Link } from './Link';
+import { Page } from './Page';
+import { ThemeStyle } from './ThemeStyle';
+
+export const allModels = {
+    Button,
+    Config,
+    Footer,
+    Header,
+    Image,
+    Link,
+    Page,
+    ThemeStyle
+};
